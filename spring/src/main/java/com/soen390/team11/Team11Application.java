a0f202d2a2d30771cca15f1be8c16325163285b3
Sunny <sunnydeng90@gmail.com>
2021-01-16 22:00:26 -0500

    Initial Project Setup

---
+++ spring/src/main/java/com/soen390/team11/Team11Application.java
@@ -0,0 +1,13 @@
+package com.soen390.team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team11Application.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/soen390/team11/Team11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen390.team11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
