a1201ee9d37956c220df2bb60cbc611588d8a10e
kimchhengheng <hchheng_178@yahoo.com>
2021-01-24 19:14:26 -0500

    change expiration time of token

--- spring/src/main/java/com/soen390/team11/security/SecurityConstant.java
+++ spring/src/main/java/com/soen390/team11/security/SecurityConstant.java
@@ -6,5 +6,5 @@ public class SecurityConstant {
     public static final String SECRET = "SecretKeyToGenJWTS";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 300_000; // in millisecond
+    public static final long EXPIRATION_TIME = 86400000; // in millisecond one day
 }
