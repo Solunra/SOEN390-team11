138aa6cf9f34176cef1ac0d407fe553b81d619cd
kimchhengheng <hchheng_178@yahoo.com>
2021-01-28 23:06:37 -0500

    authentication and authorization

---
+++ spring/src/main/java/com/soen390/team11/security/AuthenticationFilter.java
@@ -0,0 +1,70 @@
+package com.soen390.team11.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soen390.team11.dto.UserLoginRequestDto;
+import com.soen390.team11.entity.UserAccount;
+import com.soen390.team11.repository.UserAccountRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.soen390.team11.security.SecurityConstant.*;
+
+/**
+ * authentication help to define who is the person is, so we need to get the username(email) and password from client
+ */
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private UserAccountRepository userAccountRepository;
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserAccountRepository userAccountRepository) {
+        this.authenticationManager = authenticationManager;
+        this.userAccountRepository= userAccountRepository;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try{
+            UserLoginRequestDto creds = new ObjectMapper()
+                    .readValue(request.getInputStream(), UserLoginRequestDto.class);
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getUsername(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+                    );
+
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis()+ EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512,SECRET)
+                .compact();
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX+token);
+        String role = ((UserAccount) userAccountRepository.findByEmail(userName)).getRole();
+        response.addHeader("Role", role);
+    }
+}
---
+++ spring/src/main/java/com/soen390/team11/security/AuthorizationFilter.java
@@ -0,0 +1,60 @@
+package com.soen390.team11.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.soen390.team11.security.SecurityConstant.*;
+
+/**
+ * authorization help to define what the user are allow to do
+ * with the help of token we can verify if the person is authenticated or not
+ */
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    public AuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws IOException, ServletException {
+        String header = request.getHeader(HEADER_STRING);
+
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        if (token != null) {
+            token = token.replace(TOKEN_PREFIX,"");
+            String user = Jwts.parser()
+                    .setSigningKey(SECRET)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+            if (user != null) {
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
