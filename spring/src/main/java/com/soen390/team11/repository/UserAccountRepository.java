f68c98b24677586c0f7697a9e2d09bcf45fd3af1
kimchhengheng <hchheng_178@yahoo.com>
2021-01-28 23:03:20 -0500

    add dto object

--- spring/src/main/java/com/soen390/team11/repository/UserAccountRepository.java
+++ spring/src/main/java/com/soen390/team11/repository/UserAccountRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+    UserAccount findByEmail(String email);
 
 }
--- spring/src/main/java/com/soen390/team11/service/UserService.java
+++ spring/src/main/java/com/soen390/team11/service/UserService.java
@@ -1,25 +1,60 @@
 package com.soen390.team11.service;
 
 import com.soen390.team11.constant.Role;
-import com.soen390.team11.dto.UserRequestDto;
+import com.soen390.team11.dto.UserSignUpRequestDto;
 import com.soen390.team11.entity.UserAccount;
 import com.soen390.team11.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     UserAccountRepository userAccountRepository;
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void createUser(UserRequestDto userRequestDto) {
-        UserAccount userAccount = new UserAccount(userRequestDto.getUsername(),
-                bCryptPasswordEncoder.encode(userRequestDto.getPassword()),
-                userRequestDto.getEmail(), Role.CUSTOMER.toString());
+    public void createUser(UserSignUpRequestDto userSignUpRequestDto){
+        UserAccount userAccount=  new UserAccount(userSignUpRequestDto.getUsername(),
+                bCryptPasswordEncoder.encode(userSignUpRequestDto.getPassword()),
+                userSignUpRequestDto.getEmail(), Role.CUSTOMER.toString());
         userAccountRepository.save(userAccount);
+    }
+
+    /**
+     * the default admin that would display different from user
+     */
+    @Bean
+    public void addAdmin(){
+        try{
+            UserAccount admin=  new UserAccount("Admin",
+                    bCryptPasswordEncoder.encode("admin"),
+                    "admin@erp.com", Role.ADMIN.toString());
+            userAccountRepository.save(admin);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserAccount user = userAccountRepository.findByEmail(email);
+        if(user ==null) {
+            new UsernameNotFoundException("user not found");
+        }
+        return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
