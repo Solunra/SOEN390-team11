2575b8cc3d91db460728f5eca261a3aec50e8e2a
OmarSalahddine <salahddineomar@gmail.com>
2021-02-17 14:36:45 -0500

    Added th entitites for material, rawmaterial and Materialrawmaterials. The Id class is a composite to use in the Materialrawmaterials class.

---
+++ spring/src/main/java/com/soen390/team11/entity/MaterialRawMaterialsId.java
@@ -0,0 +1,55 @@
+package com.soen390.team11.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class MaterialRawMaterialsId implements Serializable {
+
+    @NotNull
+    private Long materialid;
+
+    @NotNull
+    private Long rawmaterialid;
+
+    public MaterialRawMaterialsId() {
+
+    }
+
+    public MaterialRawMaterialsId(Long materialid, Long rawmaterialid) {
+        this.materialid = materialid;
+        this.rawmaterialid = rawmaterialid;
+    }
+
+    public Long getmaterialid() {
+        return materialid;
+    }
+
+    public void setmaterialid(Long materialid) {
+        this.materialid = materialid;
+    }
+
+    public Long getrawmaterialid() {
+        return rawmaterialid;
+    }
+
+    public void setrawmaterialid(Long rawmaterialid) {
+        this.rawmaterialid = rawmaterialid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MaterialRawMaterialsId that = (MaterialRawMaterialsId) o;
+        return materialid.equals(that.materialid) && rawmaterialid.equals(that.rawmaterialid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(materialid, rawmaterialid);
+    }
+}
