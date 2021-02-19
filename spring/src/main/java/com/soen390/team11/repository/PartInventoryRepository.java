7d984def8e9e674ef3e25cf0b51c2e72c2daf69b
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-02-18 19:03:25 -0500

    add part inventory service; update repository with findBy part name method

--- spring/src/main/java/com/soen390/team11/repository/PartInventoryRepository.java
+++ spring/src/main/java/com/soen390/team11/repository/PartInventoryRepository.java
@@ -4,7 +4,9 @@ import com.soen390.team11.entity.PartInventory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartInventoryRepository extends CrudRepository<PartInventory, Long> {
-
+    List<PartInventory> findAllByName(String name);
 }
---
+++ spring/src/main/java/com/soen390/team11/service/PartInventoryService.java
@@ -0,0 +1,24 @@
+package com.soen390.team11.service;
+
+import com.soen390.team11.repository.PartInventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class PartInventoryService {
+
+    @Autowired
+    PartInventoryRepository partInventoryRepository;
+
+    public Map<Long, Integer> getPartMaterials(String partName) {
+        Map<Long, Integer> materialMap = new HashMap<>();
+        partInventoryRepository.findAllByName(partName).forEach(partInventory ->
+            materialMap.put(partInventory.getMaterialId(), partInventory.getMaterialQuantity())
+        );
+        return materialMap;
+    }
+
+
+}
