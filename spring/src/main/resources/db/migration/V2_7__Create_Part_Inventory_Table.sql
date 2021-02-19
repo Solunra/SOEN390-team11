9f918e6b345dc00014537b0897dfe93ebff8ca70
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-02-18 22:53:50 -0500

    change part inventory table from part name to part id; add unique key constraint on part id material id

--- spring/src/main/resources/db/migration/V2_7__Create_Part_Inventory_Table.sql
+++ spring/src/main/resources/db/migration/V2_7__Create_Part_Inventory_Table.sql
@@ -1,13 +1,18 @@
 CREATE TABLE team11.part_inventory
 (
-    id bigint AUTO_INCREMENT
+    id                bigint AUTO_INCREMENT
         PRIMARY KEY,
-    name varchar(255) NOT NULL,
-    material_id bigint NOT NULL,
-    material_quantity int NOT NULL,
+    part_id           bigint NOT NULL,
+    material_id       bigint NOT NULL,
+    material_quantity int    NOT NULL,
     CONSTRAINT part_inventory_material_materialid_fk
-        FOREIGN KEY (material_id) REFERENCES team11.material (materialid)
+        FOREIGN KEY (material_id) REFERENCES team11.material (materialid),
+    CONSTRAINT part_inventory_part_partid_fk
+        FOREIGN KEY (part_id) REFERENCES team11.part (partid)
 );
 
 CREATE INDEX part_inventory_name_index
-    ON team11.part_inventory (name);
+    ON team11.part_inventory (part_id);
+
+ALTER TABLE team11.part_inventory
+    ADD UNIQUE unq_part_inventory_partid_materialid (part_id, material_id);
