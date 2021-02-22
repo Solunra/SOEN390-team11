dd05f18d8b07ad4a5f58bb6652eeed92e0b8943e
Sunny <sunnydeng90@gmail.com>
2021-02-21 19:06:23 -0500

    scheduling test

---
+++ spring/src/main/java/com/soen390/team11/scheduled/ScheduleConfig.java
@@ -0,0 +1,19 @@
+package com.soen390.team11.scheduled;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+
+    public static final int ONE_MINUTE = 1000 * 60;
+    public static final int ONE_HOUR = 1000 * 60 * 60;
+
+    @Scheduled(fixedRate = ONE_MINUTE)
+    public void doesSomething()
+    {
+        System.out.println("It's been a minute");
+    }
+}
