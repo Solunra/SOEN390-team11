a8779c1ef485476da394de892034bc9634788127
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-02-23 10:34:42 -0500

    correct part material controller due to rename; improve test case assertions

--- spring/src/test/java/com/soen390/team11/controller/PartMaterialControllerTest.java
+++ spring/src/test/java/com/soen390/team11/controller/PartMaterialControllerTest.java
@@ -2,9 +2,9 @@ package com.soen390.team11.controller;
 
 import com.soen390.team11.entity.Material;
 import com.soen390.team11.entity.Part;
-import com.soen390.team11.entity.PartInventory;
+import com.soen390.team11.entity.PartMaterial;
 import com.soen390.team11.repository.MaterialRepository;
-import com.soen390.team11.repository.PartInventoryRepository;
+import com.soen390.team11.repository.PartMaterialRepository;
 import com.soen390.team11.repository.PartRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,24 +15,24 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class PartInventoryControllerTest {
+public class PartMaterialControllerTest {
 
     @Autowired
-    PartInventoryController partInventoryController;
+    PartMaterialController partMaterialController;
     @Autowired
     PartRepository partRepository;
     @Autowired
     MaterialRepository materialRepository;
     @Autowired
-    PartInventoryRepository partInventoryRepository;
+    PartMaterialRepository partMaterialRepository;
 
-    Map<String, PartInventory> partInventoryMap = new HashMap<>();
+    Map<String, PartMaterial> partMaterialMap = new HashMap<>();
     Map<String, Part> partMap = new HashMap<>();
     Map<String, Material> materialMap = new HashMap<>();
 
@@ -42,24 +42,29 @@ public class PartInventoryControllerTest {
         partMap.put("wheel", partRepository.save(new Part("wheel")));
         materialMap.put("dummy1", materialRepository.save(new Material(100L, "dummy1")));
         materialMap.put("dummy2", materialRepository.save(new Material(101L, "dummy2")));
-        PartInventory inventory1 = new PartInventory(partMap.get("saddle").getPartid(), materialMap.get("dummy1").getMaterialid(), 7);
-        PartInventory inventory2 = new PartInventory(partMap.get("saddle").getPartid(), materialMap.get("dummy2").getMaterialid(), 13);
-        partInventoryMap.put("saddle_dummy1", partInventoryRepository.save(inventory1));
-        partInventoryMap.put("saddle_dummy2", partInventoryRepository.save(inventory2));
+        PartMaterial partMaterial1 = new PartMaterial(partMap.get("saddle").getPartid(), materialMap.get("dummy1").getMaterialid(), 7);
+        PartMaterial partMaterial2 = new PartMaterial(partMap.get("saddle").getPartid(), materialMap.get("dummy2").getMaterialid(), 13);
+        partMaterialMap.put("saddle_dummy1", partMaterialRepository.save(partMaterial1));
+        partMaterialMap.put("saddle_dummy2", partMaterialRepository.save(partMaterial2));
     }
 
     @Test
-    @Order(2)
-    void retrieveMaterialsOfPartInInventory_Success() {
-        ResponseEntity<?> saddleRetrieveResponse = partInventoryController.retrievePartMaterials(partMap.get("saddle").getPartid());
-        assertNotNull(saddleRetrieveResponse.getBody());
+    void retrieveAllMaterialsOfPartInInventory_Success() {
+        ResponseEntity<?> saddleRetrieveResponse = partMaterialController.retrivePartMaterials(partMap.get("saddle").getPartid());
+
+        Map<Long, Integer> expectedPartMaterialsMap = new HashMap<>();
+        expectedPartMaterialsMap.put(materialMap.get("dummy1").getMaterialid(), 7);
+        expectedPartMaterialsMap.put(materialMap.get("dummy2").getMaterialid(), 13);
+
+        assertEquals(200, saddleRetrieveResponse.getStatusCodeValue());
+        assertEquals(expectedPartMaterialsMap, saddleRetrieveResponse.getBody());
     }
 
     @AfterAll
     public void cleanup() {
 
-        for (PartInventory partInventory : partInventoryMap.values()) {
-            partInventoryRepository.delete(partInventory);
+        for (PartMaterial partMaterial : partMaterialMap.values()) {
+            partMaterialRepository.delete(partMaterial);
         }
         for (Material material : materialMap.values()) {
             materialRepository.delete(material);
