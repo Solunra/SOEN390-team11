083ff48eb9622174ce4dac0d5f800295a6af6b42
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-03-06 17:38:15 -0500

    change test tear down behaviour to after each method #52

--- spring/src/test/java/com/soen390/team11/controller/ProductMachineryControllerTest.java
+++ spring/src/test/java/com/soen390/team11/controller/ProductMachineryControllerTest.java
@@ -3,11 +3,10 @@ package com.soen390.team11.controller;
 import com.soen390.team11.Team11Application;
 import com.soen390.team11.entity.ProductMachinery;
 import com.soen390.team11.repository.ProductMachineryRepository;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -37,7 +36,7 @@ public class ProductMachineryControllerTest {
     @Autowired
     private ProductMachineryRepository productMachineryRepository;
 
-    @AfterAll
+    @AfterEach
     public void resetDb() {
         productMachineryRepository.deleteAll();
     }
