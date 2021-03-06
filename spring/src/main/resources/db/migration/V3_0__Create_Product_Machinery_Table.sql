f09bfa8cadc799043668820865d3f08bb74c3321
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-03-06 14:52:18 -0500

    add machinery database table statements #52

---
+++ spring/src/main/resources/db/migration/V3_0__Create_Product_Machinery_Table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE team11.product_machinery
+(
+    id         varchar(255) NOT NULL
+        PRIMARY KEY,
+    name       varchar(255) NULL,
+    status     varchar(255) NULL,
+    timer      int          NULL,
+    product_id varchar(255) NULL,
+    CONSTRAINT product_machinery_product_id_fk
+        FOREIGN KEY (product_id) REFERENCES team11.product (productid)
+);
