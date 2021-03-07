22603aebb6a0cc2fbbafa327e95dc4c3d6b11432
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-03-07 12:06:50 -0500

    add create product machinery service and controller #52

--- spring/src/main/java/com/soen390/team11/controller/ProductMachineryController.java
+++ spring/src/main/java/com/soen390/team11/controller/ProductMachineryController.java
@@ -1,11 +1,10 @@
 package com.soen390.team11.controller;
 
+import com.soen390.team11.dto.ProductMachineryDto;
 import com.soen390.team11.service.ProductMachineryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/machinery")
@@ -19,6 +18,10 @@ public class ProductMachineryController {
         return ResponseEntity.ok(productMachineryService.getAllMachineries());
     }
 
-
+    @PutMapping
+    public ResponseEntity<?> createMachinery(@RequestBody ProductMachineryDto productMachineryDto) {
+        String machineryId = productMachineryService.createMachinery(productMachineryDto);
+        return ResponseEntity.ok(machineryId);
+    }
 
 }
