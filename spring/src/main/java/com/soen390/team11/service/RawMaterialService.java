3eeb60eb36b2bf1cb945a11560d792ba3f941b46
Israt Noor Kazi <kaziisratnoor@hotmial.com>
2021-03-06 22:46:38 -0500

    Added create raw material function in RawMaterialService.java

--- spring/src/main/java/com/soen390/team11/service/RawMaterialService.java
+++ spring/src/main/java/com/soen390/team11/service/RawMaterialService.java
@@ -1,5 +1,6 @@
 package com.soen390.team11.service;
 
+import com.soen390.team11.dto.RawMaterialRequestDto;
 import com.soen390.team11.entity.RawMaterial;
 import com.soen390.team11.repository.RawMaterialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,13 @@ public class RawMaterialService {
 
 
 
+
     public List<RawMaterial> getAllRawMaterial() {
-        List<RawMaterial> RawMaterials = new ArrayList<>();
-        rawmaterialRepository.findAll()
-                .forEach(RawMaterials::add);
-        return RawMaterials;
+        return (List<RawMaterial>) rawmaterialRepository.findAll();
+//        List<RawMaterial> RawMaterials = new ArrayList<>();
+//        rawmaterialRepository.findAll()
+//                .forEach(RawMaterials::add);
+//        return RawMaterials;
     }
 
 
@@ -34,6 +37,16 @@ public class RawMaterialService {
         }
     }
 
+    public String createNewRawMaterial(RawMaterialRequestDto rawMaterialRequestDto){
+        RawMaterial newRawMaterial = new RawMaterial(
+                rawMaterialRequestDto.getname(),
+                rawMaterialRequestDto.getDescription(),
+                rawMaterialRequestDto.getPrice(),
+                rawMaterialRequestDto.getUnit()
+        );
+        String rawMaterialidID = rawmaterialRepository.save(newRawMaterial).getrawmaterialid();
+        return rawMaterialidID;
+    }
 
 
 }
