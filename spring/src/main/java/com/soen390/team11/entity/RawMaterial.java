1e97f5f2479fcbc960709b1457e9ce76076cde48
Shijun Deng <dengshijun233@gmail.com>
2021-03-10 21:23:05 -0500

    modify and add javadoc to the RawMaterial class wrt the database #56

--- spring/src/main/java/com/soen390/team11/entity/RawMaterial.java
+++ spring/src/main/java/com/soen390/team11/entity/RawMaterial.java
@@ -1,11 +1,9 @@
 package com.soen390.team11.entity;
 
-
-
 import javax.persistence.*;
-import java.util.List;
+// import java.util.List;
 
-@Entity(name="rawMaterial")
+@Entity(name = "rawMaterial")
 public class RawMaterial {
 
     @Id
@@ -15,28 +13,80 @@ public class RawMaterial {
     @Column
     private String name;
 
+    @Column
+    private float cost;
+
+    /**
+     * default constructor
+     */
     public RawMaterial() {
 
     }
 
-    public RawMaterial(String rawmaterialid, String name) {
+    /**
+     * constructor with all params
+     * 
+     * @param rawmaterialid the rawmaterialid of the instance
+     * @param name          the name of the instance
+     * @param cost          the cost of the instance
+     */
+    public RawMaterial(String rawmaterialid, String name, float cost) {
         this.rawmaterialid = rawmaterialid;
         this.name = name;
+        this.cost = cost;
     }
 
+    /**
+     * getter method for the rawmaterialid of rawmaterial
+     * 
+     * @return the rawmaterialid of the instance
+     */
     public String getrawmaterialid() {
         return rawmaterialid;
     }
 
+    /**
+     * mutator method for the rawmaterialid of rawmaterial
+     * 
+     * @param rawmaterialid the desired rawmaterialid of the instance
+     */
     public void setrawmaterialid(String rawmaterialid) {
         this.rawmaterialid = rawmaterialid;
     }
 
+    /**
+     * getter method for the name of rawmaterial
+     * 
+     * @return the name of the instance
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * mutator method for the name of rawmaterial
+     * 
+     * @param name the desired name of the instance
+     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * getter method for the cost of rawmaterial
+     * 
+     * @return the cost of the instance
+     */
+    public float getCost() {
+        return cost;
+    }
+
+    /**
+     * mutator method for the cost of rawmaterial
+     * 
+     * @param cost the desired cost of the instance
+     */
+    public void setCost(float cost) {
+        this.cost = cost;
     }
 }
