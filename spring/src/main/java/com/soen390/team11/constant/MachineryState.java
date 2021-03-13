0a6ebed4ff0b16d4769face9f0553d81917d8824
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-03-13 13:37:22 -0500

    correct state transition validation #63

--- spring/src/main/java/com/soen390/team11/constant/MachineryState.java
+++ spring/src/main/java/com/soen390/team11/constant/MachineryState.java
@@ -5,17 +5,14 @@ public enum MachineryState {
 
     public static boolean validateStateTransition(MachineryState initialState, MachineryState endState) {
         return switch (initialState) {
-            case UNASSIGNED -> true;
+            case UNASSIGNED -> endState.equals(READY) || endState.equals(UNASSIGNED);
             case READY -> switch (endState) {
                 case UNASSIGNED, READY, RUNNING -> true;
                 default -> false;
             };
-            case RUNNING -> switch (endState) {
-                case READY, RUNNING, PAUSED -> true;
-                default -> false;
-            };
+            case RUNNING -> true;
             case PAUSED -> switch (endState) {
-                case RUNNING, PAUSED -> true;
+                case UNASSIGNED, RUNNING, PAUSED -> true;
                 default -> false;
             };
         };
