0d3461fbb8df05cc1aa91ede6b13ba127e2c460a
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-03-13 13:21:29 -0500

    add comments to controller #63

--- spring/src/main/java/com/soen390/team11/controller/ProductMachineryController.java
+++ spring/src/main/java/com/soen390/team11/controller/ProductMachineryController.java
@@ -1,11 +1,16 @@
 package com.soen390.team11.controller;
 
-import com.soen390.team11.constant.MachineryOp;
 import com.soen390.team11.dto.ProductMachineryDto;
 import com.soen390.team11.service.ProductMachineryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/machinery")
@@ -27,14 +32,11 @@ public class ProductMachineryController {
 
     @PostMapping("/{machineryId}/{op}")
     public ResponseEntity<?> updateMachineryStatus(@PathVariable String machineryId, @PathVariable String op) {
-        for (MachineryOp state : MachineryOp.values()) {
-            if (state.toString().equals(op)) {
-                if (productMachineryService.updateMachineryStatus(machineryId, op))
-                    return ResponseEntity.ok("Success");
-                else
-                    return ResponseEntity.notFound().build();
-            }
-        }
+
+        // try to find the machinery and update its machinery status
+        if (productMachineryService.updateMachineryStatus(machineryId, op))
+            return ResponseEntity.ok("Success");
+
         return ResponseEntity.badRequest().body("Operation not supported");
     }
 
