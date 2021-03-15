7f830494fa84170f8fe0fd3ff4036bfd1d133dad
kimchhengheng <hchheng_178@yahoo.com>
2021-03-15 16:42:02 -0400

    resolve conflict schema #58

--- spring/src/main/resources/db/migration/V3_4__Add_Costs.sql
+++ spring/src/main/resources/db/migration/V3_4__Add_Costs.sql
@@ -1,5 +1,4 @@
 -- AVOID NULL VALUES
-UPDATE product SET cost=1000.0, price=1000.0 WHERE productid LIKE 'prod-_';
 ALTER TABLE material_rawmaterials ADD qty INT;
-ALTER TABLE product ADD cost FLOAT(2) DEFAULT 0;
-ALTER TABLE product ADD price FLOAT(2) DEFAULT 0;
+ALTER TABLE product ADD cost FLOAT(2) DEFAULT 1000.0;
+ALTER TABLE product ADD price FLOAT(2) DEFAULT 1000.0;
