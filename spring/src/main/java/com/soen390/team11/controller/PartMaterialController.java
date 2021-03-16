b0cb4239ea192d286b3c9027402cd9a2e575f166
Kevin Li Zhong Hao <kevinli@Kevins-MacBook-Pro-2.local>
2021-03-16 17:50:17 -0400

    test to fix gradlew

--- spring/src/main/java/com/soen390/team11/controller/PartMaterialController.java
+++ spring/src/main/java/com/soen390/team11/controller/PartMaterialController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller for part's materials
+ * Controller for the part's materials
  */
 @RestController
 @RequestMapping("/parts")
