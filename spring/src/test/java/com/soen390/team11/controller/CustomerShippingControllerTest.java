16d2b223d140c547e962d972014e7e279e2a8a17
yuxiang <yuxiang.zhang@mail.concordia.ca>
2021-04-05 14:12:22 -0400

    add controller test for create customer address information #97

---
+++ spring/src/test/java/com/soen390/team11/controller/CustomerShippingControllerTest.java
@@ -0,0 +1,49 @@
+package com.soen390.team11.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.soen390.team11.dto.CustomerShippingDto;
+import com.soen390.team11.service.CustomerShippingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+class CustomerShippingControllerTest {
+
+    @InjectMocks
+    CustomerShippingController customerShippingController;
+
+    @Mock
+    CustomerShippingService customerShippingService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void createCompleteCustomer_Success()
+    {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+
+        String newCustomerId = "new-customer-uuid";
+        when(customerShippingService.createCustomer(any(CustomerShippingDto.class))).thenReturn(newCustomerId);
+
+        CustomerShippingDto customer = new CustomerShippingDto();
+        ResponseEntity<?> responseEntity = customerShippingController.createCustomer(customer);
+
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+        assertThat(responseEntity.getBody()).isEqualTo(newCustomerId);
+    }
+
+}
